2e990555fa17444fdfbaf96ad3142507a8168d16
N3rdy <admin@n3rdydzn.xyz>
2022-05-23 15:02:15 -0300

    2.0

---
+++ C#/05-2022/algoritmo_tabuada/algoritmo_tabuada/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Title = "Algoritmo Tabuada";
+
+int num_mulcador = 0;
+int num_fator = 0;
+int produto = 0;
+int num_inicial = 0;
+string resposta = string.Empty; 
+
+void calcular() { 
+    Console.WriteLine("Digite a tabuada que desejas meu caro");
+    Console.WriteLine("\n");
+    num_mulcador = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite até quanto você quer ("+ num_mulcador + " x B)");
+    Console.WriteLine("\n");
+    num_inicial = int.Parse(Console.ReadLine());
+
+    num_fator = num_inicial;
+
+for (int i = 0; i <= num_inicial; i++)
+{
+
+    produto = (num_mulcador * i);
+    Console.WriteLine(num_mulcador + "*" + i + "=" + produto);
+    num_fator++;
+
+}
+
+    Console.WriteLine("Deseja fazer mais contas? (S/N)");
+    Console.WriteLine("\n");
+    resposta = Console.ReadLine();
+
+}
+
+calcular();
+
+if (resposta == "S")
+{
+    Console.Clear();
+    calcular();
+}
+else
+{
+    Environment.Exit(1);
+}
