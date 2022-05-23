18e48c139ccc642a45669a7c9c92c7d2be3067c4
N3rdy <admin@n3rdydzn.xyz>
2022-05-23 15:42:54 -0300

    4.0

--- C#/03-2022/algoritmo_supermercado-main/README.md
+++
@@ -1,5 +0,0 @@
-# ✏️ Algoritmo de Super Mercado
-Algoritmo criado na escola, Apenas transformando de Pascal para C#
-
-## 📷 Screenshots
-![img](https://cdn.discordapp.com/attachments/889233196091342920/957312807647445074/unknown.png)
---
+++ C#/05-2022/algoritmo_desafio_notas/algoritmo_desafio_notas/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Title = "Desafio - Calculadora Notas";
+
+double[] notas = new double[] {0, 0, 0, 0, 0};
+
+double resultado = 0;
+
+int notita = 0;
+
+for (int i = 0; i <= 4; i++)
+{
+    notita++;
+    Console.WriteLine("Digite a Nota " + notita);
+    notas[i] = int.Parse(Console.ReadLine());
+}
+
+resultado = ((notas[0] + notas[1] + notas[2] + notas[3] + notas[4])/5);
+Console.WriteLine("A Média do aluno foi: " + resultado);
